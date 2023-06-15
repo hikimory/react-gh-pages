36aa9d20b22161c77678836d11f86aea200ce52e
Hikimory <nikita.bikon@gmail.com>
2023-06-15 15:13:35 +0300

    Update react component

--- src/Loader.js
+++ src/Loader.js
@@ -1,9 +1,9 @@
 import React from 'react';
 
-const loader = () => {
+const loader = () => (
     <div style={{display: 'flex', justifyContent: 'center', margin: '.5rem' }}>
         <div className="lds-dual-ring" />
     </div>
-};
+);
 
 export default loader;
